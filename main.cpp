e16001fb50c4078b24cdff41c4834d3fecf46647
dixx <jenzdixx@googlemail.com>
2013-12-29 19:44:11 +0100

    update main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <irrlicht.h>
-#include "Logfile.h"
 #include "Configuration.h"
+#include "Eventreceiver.h"
+#include "Logfile.h"
 
 using namespace irr;
 
@@ -9,18 +10,43 @@ s32 main()
     // Wir erstellen ein temporäres Entchen ohne Grafik, um auf das Dateisystem
     // zugreifen zu können.
     IrrlichtDevice* device = createDevice( video::EDT_NULL );
-    Logfile::getInstance( device->getFileSystem(),
+    if ( device == 0 )
+        return 1;
+    Logfile& logfile = Logfile::getInstance( device->getFileSystem(),
 #ifdef _DEBUG_MODE
     "debug.log", Logfile::DEBUG );
 #else
     "game.log", Logfile::INFO );
 #endif
-    Configuration::getInstance( device->getFileSystem() );
-    Configuration::getInstance().readConfigFile( "CONFIG.CFG" );
+    Configuration& config = Configuration::getInstance(
+            device->getFileSystem() );
+    config.readConfigFile( "CONFIG.CFG" );
     // Das hässliche temporäre Entchen wird jetzt nicht mehr benötigt,
     // deshalb zerstören wir es und geben den benutzten Speicher wieder frei.
     device->drop();
-    device = 0;
+    // Nun können wir das eigentliche Entchen mit den Config-Werten erstellen.
+    Eventreceiver& eventreceiver = Eventreceiver::getInstance();
+    eventreceiver.setEventReactionActive( false, false, false );
+    device = createDevice(
+            config.getRenderMode(),
+            config.getScreenSize(),
+            config.getColorDepht(),
+            config.isFullScreen(),
+            true,
+            config.isFullScreen(),
+            &eventreceiver
+    );
+    if ( device == 0 )
+        return 1;
+    io::IFileSystem* filesystem = device->getFileSystem();
+    video::IVideoDriver* driver = device->getVideoDriver();
+//    scene::ISceneManager* scenemanager = device->getSceneManager();
+    logfile.setNewFilesystem( filesystem );
+    config.setNewFilesystem( filesystem );
+    logfile.writeLine( Logfile::DETAIL, "3D-Entchen erfolgreich erstellt." );
+    logfile.writeLine( Logfile::DETAIL, "    Version: IrrLicht ",
+            device->getVersion() );
+    logfile.writeLine( Logfile::DETAIL, "    Treiber: ", driver->getName() );
 
 
     return 0;

9cbd671b7ef9caefb96d35ddebb85d72e1aabec8
dixx <jenzdixx@googlemail.com>
2014-01-02 12:20:12 +0100

    create class BasicLifeform

---
+++ BasicLifeform.cpp
@@ -0,0 +1,137 @@
+#include "BasicLifeform.h"
+#include "Collision.h"
+#include "Constants.h"
+#include "GenericHelperMethods.h"
+#include "Logfile.h"
+#include "ObjectManager.h"
+
+
+
+BasicLifeform::BasicLifeform(
+        const core::stringc& objectData,
+        scene::ISceneManager* smgr,
+        bool isParent
+)
+: Basic3DObject(objectData, smgr, true),
+  node_(0),
+  isMoving_(false),
+  wasMoving_(false),
+  speed_(1.0f),
+  movementDelta_(0.0f)
+{
+    if ( smgr_ == 0 )
+    {
+        Logfile::getInstance().emergencyExit(
+                "SceneManager in [BasicLifeform] nicht mehr gefunden! Abbruch."
+        );
+    }
+    init();
+    if ( !isParent )
+        deleteExtractor();
+}
+
+
+
+BasicLifeform::~BasicLifeform()
+{
+    // Niemals droppen, wenn Objekt nicht durch "create" erzeugt wurde!
+    Collision::getInstance().removeNodeFromWorld( node_ );
+    node_->removeAll(); // removes children
+    node_->remove();
+}
+
+
+
+void BasicLifeform::update( f32 frameDeltaTime )
+{
+    movementDelta_ = speed_ * frameDeltaTime;
+}
+
+
+
+scene::IAnimatedMeshSceneNode* BasicLifeform::getNode() const
+{
+    return node_;
+}
+
+
+
+f32 BasicLifeform::getCollisionRadius() const
+{
+    return collisionRadius_;
+}
+
+
+
+scene::ISceneNode* BasicLifeform::nodeInterface() const
+{
+    return static_cast<scene::ISceneNode*>(node_);
+}
+
+
+
+/* protected */
+
+
+
+scene::IAnimatedMesh* BasicLifeform::loadMesh()
+{
+    scene::IAnimatedMesh* mesh = 0;
+    if ( extractor_->tryToExtractValue( "MFILE" ) )
+    {
+        core::stringc meshFileName = extractor_->getExtractedValue();
+        if ( meshFileName.size() == 0 )
+        {
+            createLogEntry( "Leerer Mesh-Dateiname" );
+        }
+        else
+        {
+            GenericHelperMethods::getInstance().validateFileExistence(
+                    meshFileName );
+            mesh = smgr_->getMesh( meshFileName );
+        }
+    }
+    else
+    {
+        createLogEntry( "Kein Mesh-Dateiname" );
+    }
+    return mesh;
+}
+
+
+
+/* private */
+
+
+
+void BasicLifeform::init()
+{
+    core::matrix4 matrix = core::matrix4();
+
+    smgr_->getVideoDriver()->setTransform( video::ETS_WORLD, matrix );
+    node_ = smgr_->addAnimatedMeshSceneNode(
+            loadMesh(),
+            ObjectManager::getInstance().getBaseNodeByType( type_ ),
+            ObjectManager::getInstance().getBaseIdByType( type_ )
+    );
+    node_->setScale( loadScale() );
+    node_->setName( name_ );
+    node_->setRotation( loadRotation() );
+    node_->setPosition( loadPosition() + loadOffset() );
+    node_->updateAbsolutePosition();
+    node_->setMaterialTexture( 0, loadMainTexture() );
+    for ( u32 i = 0; i < node_->getMaterialCount(); ++i )
+        loadFilterAndEffects( node_->getMaterial( i ) );
+    Collision::getInstance().addAnimatedMeshNodeToWorld( node_ );
+    node_->setVisible( false );
+    calculateCollisionRadius();
+}
+
+
+
+void BasicLifeform::calculateCollisionRadius()
+{
+    core::vector3df dummy = node_->getTransformedBoundingBox().getExtent();
+    dummy.Y = 0.0f;
+    collisionRadius_ = dummy.getLength() * 0.3f;
+}
---
+++ BasicLifeform.h
@@ -0,0 +1,91 @@
+/*! \file BasicLifeform.h
+  \brief Ein Lebewesen.
+*/
+
+#ifndef _BASICLIFEFORM_HEADER
+#define _BASICLIFEFORM_HEADER
+
+#include "Basic3DObject.h"
+
+/*! \class BasicLifeform BasicLifeform.h "BasicLifeform.h"
+  \brief Ein Lebewesen.
+*/
+class BasicLifeform : public Basic3DObject
+{
+
+public:
+
+    /*! \brief Konstruktor. Erstellt ein komplettes Objekt aus der textuellen
+               Beschreibung des Lebewesens. Wurde er von einem Child der
+               Klasse aufgerufen, muss sich dieses um die Erstellung kümmern.
+      \attention Ist die Beschreibung leer oder fehlerhaft, wird ein leeres
+                 Objekt generiert, dessen 3D-Knoten-Zeiger 0 ist.
+      \param objectData (\a const \a core::stringc&) textuelle Beschreibung
+             des Objekts
+      \param smgr (\a scene::ISceneManager*) Zeiger auf den SceneManager
+      \param isParent (\a bool) Bei Vererbung auf `true` setzen, ansonsten
+             automatisch `false`
+    */
+    BasicLifeform(
+            const core::stringc& objectData,
+            scene::ISceneManager* smgr,
+            bool isParent = false
+    );
+
+    /*! \brief Destruktor.
+    */
+    virtual ~BasicLifeform();
+
+    /*! \brief Setzt z.B. die neue Position und weitere Attribute des
+               Lebewesens.
+      \param frameDeltaTime (\a const \a f32) Dauer des letzten Frames in
+             Sekunden
+      \return -
+    */
+    virtual void update( const f32 frameDeltaTime );
+
+    /*! \brief Liefert einen Zeiger auf den 3D-Knoten des Objekts, oder 0 wenn
+               kein Knoten existiert.
+      \param -
+      \return \a scene::IAnimatedMeshSceneNode* Knoten
+    */
+    scene::IAnimatedMeshSceneNode* getNode() const;
+
+    /*! \brief Liefert den Abstand zum Mittelpunkt des Objekts zurück, ab dem
+               Kollisionen abgefangen werden sollen.
+      \param -
+      \return \a const \a f32 Kollisionsradius
+    */
+    f32 getCollisionRadius() const;
+
+    /*! \brief Liefert ein Interface für den Basis-Szenenknoten des Objekts
+               zurück.
+      \param -
+      \return \a scene::ISceneNode* Interface auf den Basis-Szenenknoten
+    */
+    scene::ISceneNode* nodeInterface() const;
+
+protected:
+
+    scene::IAnimatedMeshSceneNode* node_;
+    bool isMoving_;
+    bool wasMoving_;
+    f32 speed_;
+    f32 movementDelta_;
+    f32 collisionRadius_;
+
+    scene::IAnimatedMesh* loadMesh();
+
+private:
+
+    BasicLifeform( const BasicLifeform& );
+    void operator=( const BasicLifeform& );
+
+    void init();
+    inline void calculateCollisionRadius();
+
+};
+// Ende class BasicLifeform
+
+#endif
+// Ende Header-Datei BasicLifeform

8364e67b3862ae4b912006e9c0ab5022512224e2
dixx <jenzdixx@googlemail.com>
2014-01-02 12:12:18 +0100

    create class BasicStaticObject

---
+++ BasicStaticObject.cpp
@@ -0,0 +1,123 @@
+#include "BasicStaticObject.h"
+#include "Collision.h"
+#include "Constants.h"
+#include "GenericHelperMethods.h"
+#include "ObjectManager.h"
+
+
+
+BasicStaticObject::BasicStaticObject(
+        const core::stringc& objectData,
+        scene::ISceneManager* smgr,
+        bool isParent
+)
+: Basic3DObject(objectData, smgr, true),
+  node_(0)
+{
+    init();
+    if ( !isParent )
+        deleteExtractor();
+}
+
+
+
+BasicStaticObject::~BasicStaticObject()
+{
+    if ( node_ )
+    {
+        Collision::getInstance().removeNodeFromWorld( node_ );
+        node_->remove();  // drop und delete werden implizit ausgeführt
+        node_ = 0;
+    }
+}
+
+
+
+BufferCullMeshSceneNode* BasicStaticObject::getNode() const
+{
+    return node_;
+}
+
+
+
+f32 BasicStaticObject::getCollisionRadius() const
+{
+    return 0.0f;
+}
+
+
+
+scene::ISceneNode* BasicStaticObject::nodeInterface() const
+{
+    return static_cast<scene::ISceneNode*>(node_);
+}
+
+
+
+/* protected */
+
+
+
+scene::IMesh* BasicStaticObject::loadMesh()
+{
+    scene::IMesh* mesh = 0;
+    if ( extractor_->tryToExtractValue( "MFILE" ) )
+    {
+        core::stringc meshFileName = extractor_->getExtractedValue();
+        if ( meshFileName.size() == 0 )
+        {
+            createLogEntry( "Leerer Mesh-Dateiname" );
+        }
+        else
+        {
+            GenericHelperMethods::getInstance().validateFileExistence(
+                    meshFileName );
+            mesh = smgr_->getMeshManipulator()->createMeshCopy(
+                    smgr_->getMesh( meshFileName ) );
+        }
+    }
+    else
+    {
+        createLogEntry( "Kein Mesh-Dateiname" );
+    }
+    return mesh;
+}
+
+
+
+/* private */
+
+
+
+void BasicStaticObject::init()
+{
+    scene::IMeshManipulator* meshManipulator = smgr_->getMeshManipulator();
+    core::matrix4 matrix = core::matrix4();
+
+    smgr_->getVideoDriver()->setTransform( video::ETS_WORLD, matrix );
+    scene::IMesh* dummyMesh = loadMesh();
+    if ( dummyMesh )
+    {
+        matrix.setScale( loadScale() );
+        matrix.setTranslation( loadOffset() );
+        meshManipulator->transform( dummyMesh, matrix );
+        meshManipulator->recalculateNormals( dummyMesh, true );
+        GenericHelperMethods::getInstance().pushMeshToVRAM( dummyMesh );
+        node_ = new BufferCullMeshSceneNode(
+                dummyMesh,
+                ObjectManager::getInstance().getBaseNodeByType( type_ ),
+                smgr_,
+                ObjectManager::getInstance().getBaseIdByType( type_ )
+        );
+        dummyMesh->drop();
+        node_->setName( name_ );
+        node_->setRotation( loadRotation() );
+        node_->setPosition( loadPosition() );
+        node_->updateAbsolutePosition();
+        node_->setMaterialTexture( 0, loadMainTexture() );
+        for ( u32 i = 0; i < node_->getMaterialCount(); ++i )
+            loadFilterAndEffects( node_->getMaterial( i ) );
+        Collision::getInstance().addStaticMeshNodeToWorld( node_ );
+        node_->setVisible( false );
+    }
+}
---
+++ BasicStaticObject.h
@@ -0,0 +1,84 @@
+/*! \file BasicStaticObject.h
+ \brief Ein statisches Objekt, basierend auf BufferCullMeshSceneNode.
+*/
+
+#ifndef _BASICSTATICOBJECT_HEADER
+#define _BASICSTATICOBJECT_HEADER
+
+// Linken der IRRLICHT-DLL, nur in VisualStudio nötig
+//#ifdef _IRR_WINDOWS_
+//    #pragma comment(lib, "Irrlicht.lib")
+//#endif
+
+#include <irrlicht.h>
+#include "Basic3DObject.h"
+#include "BufferCullMeshSceneNode.h"
+
+/*! \class BasicStaticObject BasicStaticObject.h "BasicStaticObject.h"
+  \brief  Ein statisches Objekt, basierend auf BufferCullMeshSceneNode.
+*/
+class BasicStaticObject : public Basic3DObject
+{
+
+public:
+
+    /*! \brief Konstruktor. Erstellt ein komplettes Objekt aus der textuellen
+               Beschreibung des statischen Objekts. Wurde er von einem Child der
+               Klasse aufgerufen, muss sich dieses um die Erstellung kümmern.
+      \attention Ist die Beschreibung leer oder fehlerhaft, wird ein leeres
+                 Objekt generiert, dessen 3D-Knoten-Zeiger 0 ist.
+      \param objectData (\a const \a core::stringc&) textuelle Beschreibung
+             des Objekts
+      \param smgr (\a scene::ISceneManager*) Zeiger auf den SceneManager
+      \param isParent (\a bool) Bei Vererbung auf `true` setzen, ansonsten
+             automatisch `false`
+    */
+    BasicStaticObject(
+            const core::stringc& objectData,
+            scene::ISceneManager* smgr,
+            bool isParent = false
+    );
+
+    /*! \brief Destruktor.
+    */
+    virtual ~BasicStaticObject();
+
+    /*! \brief Liefert einen Zeiger auf den 3D-Knoten des Objekts, oder 0 wenn
+               kein Knoten existiert.
+      \param -
+      \return \a BufferCullMeshSceneNode* Knoten
+    */
+    BufferCullMeshSceneNode* getNode() const;
+
+    /*! \brief Liefert den Abstand zum Mittelpunkt des Objekts zurück, ab dem
+               Kollisionen abgefangen werden sollen.
+      \param -
+      \return \a const \a f32 Kollisionsradius
+    */
+    f32 getCollisionRadius() const;
+
+    /*! \brief Liefert ein Interface für den Basis-Szenenknoten des Objekts
+               zurück.
+      \param -
+      \return \a scene::ISceneNode* Interface auf den Basis-Szenenknoten
+    */
+    scene::ISceneNode* nodeInterface() const;
+
+protected:
+
+    BufferCullMeshSceneNode* node_;
+
+    scene::IMesh* loadMesh();
+
+private:
+
+    BasicStaticObject( const BasicStaticObject& );
+    void operator=( const BasicStaticObject& );
+
+    void init();
+
+};
+// Ende class BasicStaticObject
+
+#endif
+// Ende Header-Datei BasicStaticObject

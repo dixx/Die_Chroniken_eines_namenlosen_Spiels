1549af8479bb695c114d42e8f84585fc3f0f9ccf
dixx <jenzdixx@googlemail.com>
2016-08-14 17:12:59 +0200

    Add first real tests for Zufall, #55

--- test/ZufallTest.cpp
+++ test/ZufallTest.cpp
@@ -1,2 +1,31 @@
 #include "catch.hpp"
 #include "../Zufall.h"
+
+TEST_CASE( "seeding" ) {
+    Zufall& subject = Zufall::getInstance();
+
+    SECTION( "seed evolves internally") {
+        subject.start( 42 );
+
+        REQUIRE( subject.getInt() == 1331530 );
+        REQUIRE( subject.getInt() != 1331530 );
+    }
+
+    SECTION( "different seed produces different result") {
+        subject.start( 43 );
+
+        REQUIRE( subject.getInt() != 1331530 );
+    }
+
+    SECTION( "same seed produces same result every time") {
+        subject.start( 42 );
+
+        REQUIRE( subject.getInt() == 1331530 );
+
+        SECTION( "resetting the seed with same value produces still the same result" ) {
+            subject.start( 42 );
+
+            REQUIRE( subject.getInt() == 1331530 );
+        }
+    }
+}

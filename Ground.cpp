4005e075d83eea66c11fd9024e4b14b4f4de7a23
dixx <jenzdixx@googlemail.com>
2016-12-18 13:29:03 +0100

    Refactor Ground irr namespace usage

--- Ground.cpp
+++ Ground.cpp
@@ -13,7 +13,7 @@
 
 
 
-Ground& Ground::getInstance( IrrlichtDevice* device )
+Ground& Ground::getInstance( irr::IrrlichtDevice* device )
 {
     static Ground instance( device );
     return instance;
@@ -24,47 +24,47 @@ Ground& Ground::getInstance( IrrlichtDevice* device )
 void Ground::load( const char* mapfilename )
 {
     Logfile& logfile = Logfile::getInstance();
-    core::array<core::stringc> tileList;
-    u32 position = 0;
-    s32 tileX = 0;
-    s32 tileZ = 0;
+    irr::core::array<irr::core::stringc> tileList;
+    irr::u32 position = 0;
+    irr::s32 tileX = 0;
+    irr::s32 tileZ = 0;
 
     // Kartenteile-Rohdaten laden
     GenericHelperMethods::getInstance().validateFileExistence( mapfilename );
     Scripting::getInstance().getObjectDataFromScript( mapfilename ).split( tileList, "\n" );
 
     // berechne Grid-Größe, prüfe Positionen auf Gültigkeit
-    for ( register u32 i = 0; i < tileList.size(); ++i )
+    for ( register irr::u32 i = 0; i < tileList.size(); ++i )
     {
         ObjectParamsExtractor* extractor = new ObjectParamsExtractor( tileList[ i ] );
         if ( extractor->tryToExtractValue( "POSXZ", "x", 0 ) )
-            tileX = core::strtol10( extractor->getExtractedValue().c_str() );
+            tileX = irr::core::strtol10( extractor->getExtractedValue().c_str() );
         else
             exitWithLogEntry( "Fehlender X-Positionswert", mapfilename );
         if ( extractor->tryToExtractValue( "POSXZ", "x", 1 ) )
-            tileZ = core::strtol10( extractor->getExtractedValue().c_str() );
+            tileZ = irr::core::strtol10( extractor->getExtractedValue().c_str() );
         else
             exitWithLogEntry( "Fehlender Z-Positionswert", mapfilename );
         if ( tileX < 0 || tileZ < 0 )
             exitWithLogEntry( "Negativer Positionswert", mapfilename );
         if ( tileX > 100 || tileZ > 100 )
             exitWithLogEntry( "Positionswert größer als 100", mapfilename );
-        gridWidth_ = core::max_( static_cast<s32>( gridWidth_ ), tileX );
-        gridDepth_ = core::max_( static_cast<s32>( gridDepth_ ), tileZ );
+        gridWidth_ = irr::core::max_( static_cast<irr::s32>( gridWidth_ ), tileX );
+        gridDepth_ = irr::core::max_( static_cast<irr::s32>( gridDepth_ ), tileZ );
         delete extractor;
     }
     gridWidth_ += 1;
     gridDepth_ += 1;
 
     // initialisiere Grid
-    for ( register u32 i = 0; i <= gridWidth_ * gridDepth_; ++i )
+    for ( register irr::u32 i = 0; i <= gridWidth_ * gridDepth_; ++i )
         mapTiles_.push_back( 0 );
 
     // erstelle Grid mit Kartenteilen
 #ifdef _DEBUG_MODE
 u32 now = device_->getTimer()->getRealTime();
 #endif
-    for ( register u32 i = 0; i < tileList.size(); ++i )
+    for ( register irr::u32 i = 0; i < tileList.size(); ++i )
     {
         MapTile* tile = new MapTile( tileList[ i ], smgr_, SECTORDIMENSION );
         if ( tile->getNode() == 0 )
@@ -75,12 +75,12 @@ u32 now = device_->getTimer()->getRealTime();
             logfile.emergencyExit( "Kartenteil konnte nicht erzeugt werden! Abbruch." );
         }
         position = tile->getX() + tile->getZ() * gridWidth_;
-        maxHeight_ = core::max_( maxHeight_, tile->getNode()->getTransformedBoundingBox().MaxEdge.Y );
-        minHeight_ = core::min_( minHeight_, tile->getNode()->getTransformedBoundingBox().MinEdge.Y );
+        maxHeight_ = irr::core::max_( maxHeight_, tile->getNode()->getTransformedBoundingBox().MaxEdge.Y );
+        minHeight_ = irr::core::min_( minHeight_, tile->getNode()->getTransformedBoundingBox().MinEdge.Y );
         mapTiles_[ position ] = tile;
     }
 #ifdef _DEBUG_MODE
-    core::stringw logText = L"";
+    irr::core::stringw logText = L"";
     logText += tileList.size();
     logText += " Kartenteile in ";
     logText += device_->getTimer()->getRealTime() - now;
@@ -120,25 +120,25 @@ void Ground::update()
 {
     if ( updateTimer_->isFull() )
     {
-        s32 sumX = 0;
-        s32 sumZ = 0;
-        s32 pos  = 0;
-        core::vector3df camPos = smgr_->getActiveCamera()->getAbsolutePosition();
-        updateSectorX_ = core::floor32( camPos.X / SECTORDIMENSION.Width );
-        updateSectorZ_ = core::floor32( camPos.Z / SECTORDIMENSION.Height );
+        irr::s32 sumX = 0;
+        irr::s32 sumZ = 0;
+        irr::s32 pos  = 0;
+        irr::core::vector3df camPos = smgr_->getActiveCamera()->getAbsolutePosition();
+        updateSectorX_ = irr::core::floor32( camPos.X / SECTORDIMENSION.Width );
+        updateSectorZ_ = irr::core::floor32( camPos.Z / SECTORDIMENSION.Height );
         ObjectManager& objectManager = ObjectManager::getInstance();
 #ifdef _DEBUG_MODE
         visibleNodeCount_ = 0;
 #endif
-        for ( register s32 z = -2; z < 3; ++z )
+        for ( register irr::s32 z = -2; z < 3; ++z )
         {
             sumZ = updateSectorZ_ + z;
-            if ( sumZ < 0 || sumZ >= static_cast<s32>( gridDepth_ ) )
+            if ( sumZ < 0 || sumZ >= static_cast<irr::s32>( gridDepth_ ) )
                 continue;
-            for ( register s32 x = -2; x < 3; ++x )
+            for ( register irr::s32 x = -2; x < 3; ++x )
             {
                 sumX = updateSectorX_ + x;
-                if ( sumX < 0 || sumX >= static_cast<s32>( gridWidth_ ) )
+                if ( sumX < 0 || sumX >= static_cast<irr::s32>( gridWidth_ ) )
                     continue;
                 pos = sumX + sumZ * gridWidth_;
                 if ( mapTiles_[ pos ] == 0 )
@@ -168,9 +168,9 @@ void Ground::update()
 
 
 
-f32 Ground::getHeight( const f32 x, const f32 z )
+irr::f32 Ground::getHeight( const irr::f32 x, const irr::f32 z )
 {
-    ray_ = core::line3df( x, maxHeight_ + 1.0f, z, x, minHeight_ - 1.0f, z );
+    ray_ = irr::core::line3df( x, maxHeight_ + 1.0f, z, x, minHeight_ - 1.0f, z );
     if ( Collision::getInstance().isRayIntersectingWithWorld( ray_, ObjectManager::getInstance().walkableNodes ) )
         return Collision::getInstance().collisionPoint.Y;
     else
@@ -179,13 +179,13 @@ f32 Ground::getHeight( const f32 x, const f32 z )
 
 
 
-core::vector3df& Ground::getHeightFromPosition( const core::vector3df& position, f32 heightOffset )
+irr::core::vector3df& Ground::getHeightFromPosition( const irr::core::vector3df& position, irr::f32 heightOffset )
 {
-    ray_ = core::line3df(
+    ray_ = irr::core::line3df(
             position.X, position.Y + heightOffset, position.Z,
             position.X,         minHeight_ - 1.0f, position.Z
     );
-    endPosition_ = core::vector3df( position.X, maxHeight_, position.Z );
+    endPosition_ = irr::core::vector3df( position.X, maxHeight_, position.Z );
     if ( Collision::getInstance().isRayIntersectingWithWorld( ray_, ObjectManager::getInstance().walkableNodes ) )
         endPosition_ = Collision::getInstance().collisionPoint;
     return endPosition_;
@@ -193,9 +193,9 @@ core::vector3df& Ground::getHeightFromPosition( const core::vector3df& position,
 
 
 
-f32 Ground::getHeightRanged( const f32 x, const f32 z )
+irr::f32 Ground::getHeightRanged( const irr::f32 x, const irr::f32 z )
 {
-    ray_ = core::line3df( x, maxHeight_ + 1.0f, z, x, minHeight_ - 1.0f, z );
+    ray_ = irr::core::line3df( x, maxHeight_ + 1.0f, z, x, minHeight_ - 1.0f, z );
 //        if ( Collision::getInstance().isRayIntersectingWithWorld( this->ray,
 //                ObjectManager::getInstance().walkableNodesInRange ) )
     if ( Collision::getInstance().isRayIntersectingWithWalkableNodesAroundHero( ray_ ) )
@@ -206,13 +206,13 @@ f32 Ground::getHeightRanged( const f32 x, const f32 z )
 
 
 
-core::vector3df& Ground::getHeightFromPositionRanged( const core::vector3df& position, f32 heightOffset )
+irr::core::vector3df& Ground::getHeightFromPositionRanged( const irr::core::vector3df& position, irr::f32 heightOffset )
 {
-    ray_ = core::line3df(
+    ray_ = irr::core::line3df(
             position.X, position.Y + heightOffset, position.Z,
             position.X,         minHeight_ - 1.0f, position.Z
     );
-    endPosition_ = core::vector3df( position.X, maxHeight_, position.Z );
+    endPosition_ = irr::core::vector3df( position.X, maxHeight_, position.Z );
 //    if ( Collision::getInstance().isRayIntersectingWithWorld( this->ray,
 //            ObjectManager::getInstance().walkableNodesInRange ) )
     if ( Collision::getInstance().isRayIntersectingWithWalkableNodesAroundHero( ray_ ) )
@@ -228,9 +228,9 @@ void Ground::switchDebugMode()
     ( debugCounter_ == 4 ) ? debugCounter_ = 0 : ++debugCounter_;
     BufferCullMeshSceneNode* helperNode = 0;
     MapTile* helperTile = 0;
-    for ( register u32 z = 0; z < gridDepth_; ++z )
+    for ( register irr::u32 z = 0; z < gridDepth_; ++z )
     {
-        for ( register u32 x = 0; x < gridWidth_; ++x )
+        for ( register irr::u32 x = 0; x < gridWidth_; ++x )
         {
             helperTile = mapTiles_[ x + z * gridWidth_ ];
             if ( !helperTile ) continue;
@@ -241,26 +241,26 @@ void Ground::switchDebugMode()
             {
                 case 0:
                     helperNode->setDebugDataVisible( 0 );
-                    helperNode->setMaterialFlag( video::EMF_WIREFRAME, false );
+                    helperNode->setMaterialFlag( irr::video::EMF_WIREFRAME, false );
                     break;
                 case 1:
-                    helperNode->setDebugDataVisible( scene::EDS_NORMALS );
+                    helperNode->setDebugDataVisible( irr::scene::EDS_NORMALS );
                     break;
                 case 2:
                     helperNode->setDebugDataVisible( 0 );
-                    helperNode->setMaterialFlag( video::EMF_WIREFRAME, true );
+                    helperNode->setMaterialFlag( irr::video::EMF_WIREFRAME, true );
                     break;
                 case 3:
-                    helperNode->setDebugDataVisible( scene::EDS_BBOX );
-                    helperNode->setMaterialFlag( video::EMF_WIREFRAME, false );
+                    helperNode->setDebugDataVisible( irr::scene::EDS_BBOX );
+                    helperNode->setMaterialFlag( irr::video::EMF_WIREFRAME, false );
                     break;
                 case 4:
-                    helperNode->setDebugDataVisible( scene::EDS_BBOX_BUFFERS );
-                    helperNode->setMaterialFlag( video::EMF_WIREFRAME, false );
+                    helperNode->setDebugDataVisible( irr::scene::EDS_BBOX_BUFFERS );
+                    helperNode->setMaterialFlag( irr::video::EMF_WIREFRAME, false );
                     break;
                 default:
                     helperNode->setDebugDataVisible( 0 );
-                    helperNode->setMaterialFlag( video::EMF_WIREFRAME, false );
+                    helperNode->setMaterialFlag( irr::video::EMF_WIREFRAME, false );
                     break;
             }
         }
@@ -274,9 +274,9 @@ void Ground::switchDebugMode()
 
 
 
-Ground::Ground( IrrlichtDevice* device )
+Ground::Ground( irr::IrrlichtDevice* device )
 : device_(device),
-  SECTORDIMENSION(core::dimension2df( 400.0f, 400.0f )),
+  SECTORDIMENSION(irr::core::dimension2df( 400.0f, 400.0f )),
   gridWidth_(0),
   gridDepth_(0),
   ray_(VEC_3DF_NULL, VEC_3DF_NULL),
@@ -312,7 +312,7 @@ void Ground::clearArrays()
 {
     if ( mapTiles_.size() > 0 )
     {
-        for ( register u32 i = 0; i < mapTiles_.size(); ++i )
+        for ( register irr::u32 i = 0; i < mapTiles_.size(); ++i )
         {
             if ( mapTiles_[ i ] )
             {
@@ -326,7 +326,7 @@ void Ground::clearArrays()
 
 
 
-void Ground::exitWithLogEntry( const core::stringc& message, const char* affectedFile )
+void Ground::exitWithLogEntry( const irr::core::stringc& message, const char* affectedFile )
 {
     clearArrays();
     TimerManager::getInstance().removeTimer( updateTimer_ );

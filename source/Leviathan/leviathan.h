6d315b7dba7a461820bf2bf78d3988fd6be6782a
dixx <jenzdixx@googlemail.com>
2016-12-19 16:19:12 +0100

    Add Leviathan Game Engine header, #67

---
+++ source/Leviathan/leviathan.h
@@ -0,0 +1,20 @@
+/*! \file leviathan.h
+ *  \brief Interface der 'Leviathan Engine'.
+ *         Dies ist der einzige Header der inkludiert werden muss.
+ */
+
+#ifndef _LEVIATHAN_HEADER
+#define _LEVIATHAN_HEADER
+
+#include <irrlicht.h>
+
+//! Alle bedienbaren Teile der Leviathan Game Engine findest du hier.
+namespace leviathan
+{
+    //! Grundlegende Bestandteile der Engine.
+    namespace core
+    {
+    }
+}
+
+#endif

1932f9506e6b25056831397d3f20a6253a00997a
dixx <jenzdixx@googlemail.com>
2016-12-19 18:38:49 +0100

    Add more tests for Timer, #67

--- test/TimerTest.cpp
+++ test/TimerTest.cpp
@@ -1,13 +1,19 @@
 #include "catch.hpp"
 #include "../source/Leviathan/leviathan.h"
-#include <time.h>
 
 TEST_CASE( "Timer: architecture" ) {
-    SECTION( "it is not a static instance" ) {
-        leviathan::core::Timer timer1( 1.0f );
-        leviathan::core::Timer timer2( 1.0f );
+    leviathan::core::Timer timer( 2.0f );
 
-        REQUIRE( &timer1 != &timer2 );
+    SECTION( "it is not a static instance" ) {
+        leviathan::core::Timer timer2( 2.0f );
+
+        REQUIRE( &timer != &timer2 );
+    }
+
+    SECTION( "tick() starts chains" ) {
+        timer.start();
+
+        REQUIRE( timer.tick( 1.1f ).tick( 1.1f ).isFull() );
     }
 }
 
@@ -25,6 +31,14 @@ TEST_CASE( "Timer: ticking" ) {
         SECTION( "when it is paused" ) {
             timer.start();
             timer.pause();
+            timer.tick( 2.2f );
+
+            REQUIRE_FALSE( timer.isFull() );
+        }
+
+        SECTION( "when it is stopped" ) {
+            timer.start();
+            timer.stop();
             timer.tick( 2.2f );
 
             REQUIRE_FALSE( timer.isFull() );
@@ -47,6 +61,70 @@ TEST_CASE( "Timer: ticking" ) {
             timer.tick( 2.2f );
 
             REQUIRE( timer.isFull() );
+        }
+
+        SECTION( "when it is restarted" ) {
+            timer.start();
+            timer.restart();
+            timer.tick( 2.2f );
+
+            REQUIRE( timer.isFull() );
+        }
+    }
+
+    SECTION( "it can tick more than once" ) {
+        timer.start();
+
+        timer.tick( 0.6f );
+        timer.tick( 0.6f );
+        REQUIRE_FALSE( timer.isFull() );
+
+        timer.tick( 0.6f );
+        timer.tick( 0.6f );
+        REQUIRE( timer.isFull() );
+    }
+}
+
+TEST_CASE( "Timer: states" ) {
+    leviathan::core::Timer timer( 2.0f );
+
+    SECTION( "it is not running" ) {
+        SECTION( "when it is newly created" ) {
+            REQUIRE_FALSE( timer.isRunning() );
+        }
+
+        SECTION( "when it is stopped" ) {
+            timer.start();
+            timer.stop();
+
+            REQUIRE_FALSE( timer.isRunning() );
+        }
+    }
+
+
+    SECTION( "it is running" ) {
+        SECTION( "when it is started" ) {
+            timer.start();
+
+            REQUIRE( timer.isRunning() );
+
+            SECTION( "and when it is paused" ) {
+                timer.pause();
+
+                REQUIRE( timer.isRunning() );
+
+                SECTION( "and when it is resumed" ) {
+                    timer.resume();
+
+                    REQUIRE( timer.isRunning() );
+                }
+            }
+
+            SECTION( "and when it is restarted" ) {
+                timer.restart();
+
+                REQUIRE( timer.isRunning() );
+            }
         }
     }
 }

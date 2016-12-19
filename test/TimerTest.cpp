d67de99c185802b5a51644e2182e757913ea41f1
dixx <jenzdixx@googlemail.com>
2016-12-19 22:09:54 +0100

    Add more tests for Timer, #67

--- test/TimerTest.cpp
+++ test/TimerTest.cpp
@@ -17,6 +17,37 @@ TEST_CASE( "Timer: architecture" ) {
     }
 }
 
+TEST_CASE( "Timer: max value" ) {
+
+    SECTION( "zero is not forbidden" ) {
+        leviathan::core::Timer timer( 0.0f );
+
+        REQUIRE( irr::core::equals( timer.getMaxValue(), 0.0f ) );
+        REQUIRE( timer.isFull() );
+    }
+
+    SECTION( "negative values are not forbidden" ) {
+        leviathan::core::Timer timer( -10.0f );
+
+        REQUIRE( irr::core::equals( timer.getMaxValue(), -10.0f ) );
+        REQUIRE( timer.isFull() );
+    }
+
+    SECTION( "values are exactly met" ) {
+        leviathan::core::Timer timer( 1.0f );
+        timer.start();
+
+        REQUIRE( irr::core::equals( timer.getMaxValue(), 1.0f ) );
+        REQUIRE_FALSE( timer.isFull() );
+
+        timer.tick( 1.0f - irr::core::ROUNDING_ERROR_f32 );
+        REQUIRE_FALSE( timer.isFull() );
+
+        timer.tick( irr::core::ROUNDING_ERROR_f32 );
+        REQUIRE( timer.isFull() );
+    }
+}
+
 TEST_CASE( "Timer: ticking" ) {
     leviathan::core::Timer timer( 2.0f );
 
@@ -83,6 +114,25 @@ TEST_CASE( "Timer: ticking" ) {
         timer.tick( 0.6f );
         REQUIRE( timer.isFull() );
     }
+
+    SECTION( "it can tick backwards" ) {
+        timer.start();
+
+        timer.tick( -10.0f );
+        REQUIRE_FALSE( timer.isFull() );
+
+        timer.tick( 11.9f );
+        REQUIRE_FALSE( timer.isFull() );
+
+        timer.tick( 0.2f );
+        REQUIRE( timer.isFull() );
+
+        SECTION( "but it stays full if once reached" ) {
+            timer.tick( -10.0f );
+
+            REQUIRE( timer.isFull() );
+        }
+    }
 }
 
 TEST_CASE( "Timer: states" ) {
@@ -100,7 +150,6 @@ TEST_CASE( "Timer: states" ) {
             REQUIRE_FALSE( timer.isRunning() );
         }
     }
-
 
     SECTION( "it is running" ) {
         SECTION( "when it is started" ) {
@@ -125,6 +174,49 @@ TEST_CASE( "Timer: states" ) {
 
                 REQUIRE( timer.isRunning() );
             }
+        }
+    }
+
+    SECTION( "it is paused" ) {
+        SECTION( "when it is paused after being started" ) {
+            timer.start();
+            timer.pause();
+
+            REQUIRE( timer.isPaused() );
+        }
+    }
+
+    SECTION( "it is not paused" ) {
+        SECTION( "when it is newly created" ) {
+            REQUIRE_FALSE( timer.isPaused() );
+        }
+
+        SECTION( "when it is started" ) {
+            timer.start();
+
+            REQUIRE_FALSE( timer.isPaused() );
+        }
+
+        SECTION( "when it is restarted" ) {
+            timer.start();
+            timer.restart();
+
+            REQUIRE_FALSE( timer.isPaused() );
+        }
+
+        SECTION( "when it is resumed" ) {
+            timer.start();
+            timer.pause();
+            timer.resume();
+
+            REQUIRE_FALSE( timer.isPaused() );
+        }
+
+        SECTION( "when it is stopped" ) {
+            timer.start();
+            timer.stop();
+
+            REQUIRE_FALSE( timer.isPaused() );
         }
     }
 }

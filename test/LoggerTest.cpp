9275c8e57943ba71948c846cdc6d28cf4543fa22
dixx <jenzdixx@googlemail.com>
2017-01-28 21:31:28 +0100

    Add Testhelper

--- test/LoggerTest.cpp
+++ test/LoggerTest.cpp
@@ -1,110 +1,86 @@
 #include "catch.hpp"
 #include "leviathan.h"
-
-irr::IrrlichtDevice* createGraphicEngine()
-{
-    irr::SIrrlichtCreationParameters params;
-    params.DriverType = irr::video::EDT_NULL;
-    params.LoggingLevel = irr::ELL_WARNING;
-    return irr::createDeviceEx( params );
-}
-
-irr::core::stringc readFile( irr::io::path fileName, irr::io::IFileSystem* fileSystem )
-{
-    irr::io::IReadFile* file = fileSystem->createAndOpenFile( fileName );
-    irr::u32 size = (irr::u32)file->getSize();
-    irr::u8 buffer[ size + 4 ];
-    file->read( &buffer, size );
-    file->drop();
-    return (irr::core::stringc)buffer;
-}
-
-irr::u32 getFileSize( irr::io::path fileName, irr::io::IFileSystem* fileSystem )
-{
-    irr::io::IReadFile* file = fileSystem->createAndOpenFile( fileName );
-    irr::u32 size = (irr::u32)file->getSize();
-    file->drop();
-    return size;
-}
+#include "Testhelper.h"
 
 TEST_CASE( "Logger: init" ) {
-    irr::IrrlichtDevice* graphicEngine = createGraphicEngine();
-    irr::io::IFileSystem* fileSystem = graphicEngine->getFileSystem();
+    Testhelper testhelper;
     const irr::io::path logFileName = "testlogfile.log";
-    leviathan::core::Logger sample( fileSystem, graphicEngine->getTimer(), logFileName, leviathan::core::Logger::INFO );
+    leviathan::core::Logger sample(
+        testhelper.getFileSystem(),
+        testhelper.getGraphicEngine()->getTimer(),
+        logFileName,
+        leviathan::core::Logger::INFO
+    );
     sample.flush();
 
     SECTION( "it creates a logfile" ) {
-        REQUIRE( fileSystem->existFile( logFileName ) );
+        REQUIRE( testhelper.existFile( logFileName ) );
 
-        irr::u32 size = getFileSize( logFileName, fileSystem );
+        irr::u32 size = testhelper.getFileSize( logFileName );
 
         SECTION( "by overwriting an existing logfile" ) {
             leviathan::core::Logger subject(
-                    fileSystem,
-                    graphicEngine->getTimer(),
+                    testhelper.getFileSystem(),
+                    testhelper.getGraphicEngine()->getTimer(),
                     logFileName,
                     leviathan::core::Logger::INFO
             );
             subject.flush();
-            irr::u32 newSize = getFileSize( logFileName, fileSystem );
+            irr::u32 newSize = testhelper.getFileSize( logFileName );
             REQUIRE( newSize == size );
         }
         SECTION( "by appending to an existing logfile" ) {
             leviathan::core::Logger subject(
-                    fileSystem,
-                    graphicEngine->getTimer(),
+                    testhelper.getFileSystem(),
+                    testhelper.getGraphicEngine()->getTimer(),
                     logFileName,
                     leviathan::core::Logger::INFO,
                     true
             );
             subject.flush();
-            irr::u32 newSize = getFileSize( logFileName, fileSystem );
+            irr::u32 newSize = testhelper.getFileSize( logFileName );
             REQUIRE( newSize == size * 2 );
         }
         SECTION( "and writes the global logging level into it" ) {
-            irr::core::stringc content = readFile( logFileName, fileSystem );
+            irr::core::stringc content =testhelper. readFile( logFileName );
             REQUIRE( content.find("] LogLevel: Info") > -1 );
         }
     }
     SECTION( "multiple logfiles may exist" ) {
         const irr::io::path anotherLogFileName = "testlogfile2.log";
         leviathan::core::Logger sample2(
-                fileSystem,
-                graphicEngine->getTimer(),
+                testhelper.getFileSystem(),
+                testhelper.getGraphicEngine()->getTimer(),
                 anotherLogFileName,
                 leviathan::core::Logger::INFO
         );
-        REQUIRE( fileSystem->existFile( logFileName ) );
-        REQUIRE( fileSystem->existFile( anotherLogFileName ) );
+        REQUIRE( testhelper.existFile( logFileName ) );
+        REQUIRE( testhelper.existFile( anotherLogFileName ) );
     }
-
-    graphicEngine->drop();
 }
 
 TEST_CASE( "Logger: logging" ) {
-    irr::IrrlichtDevice* graphicEngine = createGraphicEngine();
-    irr::io::IFileSystem* fileSystem = graphicEngine->getFileSystem();
+    Testhelper testhelper;
     const irr::io::path logFileName = "testlogfile.log";
 
     SECTION( "it writes text into the logfile" ) {
         leviathan::core::Logger subject(
-                fileSystem,
-                graphicEngine->getTimer(),
+                testhelper.getFileSystem(),
+                testhelper.getGraphicEngine()->getTimer(),
                 logFileName,
                 leviathan::core::Logger::INFO
         );
         subject.text = "Kilroy wuz here";
         subject.write();
         subject.flush();
-        irr::core::stringc content = readFile( logFileName, fileSystem );
+        irr::core::stringc content = testhelper.readFile( logFileName );
         REQUIRE( content.find( "] Kilroy wuz here" ) > -1 );
 
         SECTION( "with special characters" ) {
             subject.text = "german umlauts: äöüß, some other things: >_%&$§?!@|";
             subject.write();
             subject.flush();
-            content = readFile( logFileName, fileSystem );
+            content = testhelper.readFile( logFileName );
             REQUIRE( content.find( "äöüß" ) > -1 );
             REQUIRE( content.find( ">_%&$§?!@|" ) > -1 );
         }
@@ -114,14 +90,19 @@ TEST_CASE( "Logger: logging" ) {
             subject.text = "another line.";
             subject.write();
             subject.flush();
-            content = readFile( logFileName, fileSystem );
+            content = testhelper.readFile( logFileName );
             irr::s32 firstIndex = content.find( "line." );
             REQUIRE( firstIndex > -1 );
             REQUIRE( content.find( "line.", (irr::u32)firstIndex + 1 ) > -1 );
         }
     }
     SECTION( "it handles different logLevels" ) {
-        leviathan::core::Logger subject( fileSystem, graphicEngine->getTimer(), logFileName, leviathan::core::Logger::DETAIL );
+        leviathan::core::Logger subject(
+            testhelper.getFileSystem(),
+            testhelper.getGraphicEngine()->getTimer(),
+            logFileName,
+            leviathan::core::Logger::DETAIL
+        );
         subject.text = "a line just for information";
         subject.write( leviathan::core::Logger::INFO );
         subject.text = "a line full of details";
@@ -131,12 +112,10 @@ TEST_CASE( "Logger: logging" ) {
         subject.text = "a line hopefully never ever written";
         subject.write( leviathan::core::Logger::ALL );
         subject.flush();
-        irr::core::stringc content = readFile( logFileName, fileSystem );
+        irr::core::stringc content = testhelper.readFile( logFileName );
         REQUIRE( content.find( "information" ) > -1 );
         REQUIRE( content.find( "details" ) > -1 );
         REQUIRE_FALSE( content.find( "debugging" ) > -1 );
         REQUIRE_FALSE( content.find( "never ever" ) > -1 );
     }
-
-    graphicEngine->drop();
 }

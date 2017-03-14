fbdb934b865939d09f9265d1b06ff2fe4dba8c04
dixx <jenzdixx@googlemail.com>
2017-03-15 00:30:16 +0100

    Add thoughts to Configuration tests

--- test/ConfigurationTest.cpp
+++ test/ConfigurationTest.cpp
@@ -2,16 +2,35 @@
 #include "leviathan.h"
 #include "Testhelper.h"
 
+// - INI format with # and ; as allowed comment chars
+// - inline comments are forbidden
+// - blank lines, leading and trailing spaces allowed
+// - string values must have ""
+// - enum values are allowed (only /[\w_]/)
+// - boolean values are `true` and `false`
+// - float values may not have leading zeroes, and must have a `.` in them
+// - class should get and parse content only once, store pairs temporarily in a map/dict
+// - test many erroneous config files, class should log and exit
+
 TEST_CASE( "Configuration: read from file" ) {
+    const irr::io::path configFileName = "testconfigfile.ini";
+
+    // preconditions: create contents and write to file
+
+    SECTION( "comments are ignored" ) {}
+    SECTION( "unneccessary white spaces are ignored" ) {}
+
+    SECTION( "all relevant values can be read" ) {}
+    SECTION( "it has default values" ) {}
     SECTION( "it uses some of the values in the graphic engine" ) {}
-    SECTION( "it can read user defined sections" ) {}
+
+    // SECTION( "it can read user defined sections" ) {}
 }
 
 TEST_CASE( "Configuration: write to file" ) {
-    SECTION( "it writes back graphic engine values" ) {}
-    SECTION( "it writes back the user defined sections that were read before" ) {}
-}
+    const irr::io::path configFileName = "testconfigfile.ini";
 
-TEST_CASE( "Configuration: getter") {
-    SECTION( "it returns" ) {}
+    SECTION( "it writes back all sections and values" ) {}
+
+    // SECTION( "it writes back the user defined sections that were read before" ) {}
 }

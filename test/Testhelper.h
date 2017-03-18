e0c52e4e43c60dcb24edd2f652667c71827e1681
dixx <jenzdixx@googlemail.com>
2017-03-18 13:31:43 +0100

    Add Testhelper::writeFile() method

--- test/Testhelper.h
+++ test/Testhelper.h
@@ -11,6 +11,7 @@ public:
     irr::IrrlichtDevice* getGraphicEngine();
     irr::io::IFileSystem* getFileSystem();
     irr::core::stringc readFile( irr::io::path fileName );
+    void writeFile( irr::io::path fileName, const irr::core::stringc& content );
     irr::u32 getFileSize( irr::io::path fileName );
     bool existFile( irr::io::path fileName );
 private:

415d708787a278d1d3ab04d918dd55d7d44ec7a4
dixx <jenzdixx@googlemail.com>
2017-03-30 22:31:23 +0200

    Refactor CMakeList, divide into functional files

---
+++ cmake/FindLua.cmake
@@ -0,0 +1,18 @@
+# TODO find existing LUA
+
+set(LUA_DIR ${CMAKE_SOURCE_DIR}/dependencies/lua-5.2.4)
+set(LUA_INCLUDE_DIRS ${LUA_DIR}/include)
+
+if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+  set(LUA_LIBRARY ${LUA_DIR}/liblua.a) # the one >200kB! If not exist, build it.
+  set(LUA_BINARY_LIBRARY ${LUA_DIR}/bin/lua52.dll)
+endif(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+
+if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+  set(LUA_LIBRARY lua dl)
+endif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+
+#if(APPLE)
+#endif(APPLE)
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DNULL=0") # sadly this is required by LUA

76ae8d3cf829d3e3f7bb91a4485e774381d193ee
dixx <jenzdixx@googlemail.com>
2017-04-02 01:00:49 +0200

    Update Lua version

--- cmake/FindLua.cmake
+++ cmake/FindLua.cmake
@@ -4,8 +4,8 @@ set(LUA_DIR ${CMAKE_SOURCE_DIR}/dependencies/lua-5.3.2)
 set(LUA_INCLUDE_DIRS ${LUA_DIR}/include)
 
 if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
-  set(LUA_LIBRARY ${LUA_DIR}/liblua.a) # the one >200kB! If not exist, build it.
-  set(LUA_BINARY_LIBRARY ${LUA_DIR}/bin/lua52.dll)
+  set(LUA_LIBRARY ${LUA_DIR}/liblua53.a) # the one >200kB! If not exist, build it.
+  set(LUA_BINARY_LIBRARY ${LUA_DIR}/bin/lua53.dll)
 endif(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
 
 if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
@@ -17,3 +17,4 @@ endif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
 #endif(APPLE)
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DNULL=0") # sadly this is required by LUA
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DLUA_C89_NUMBERS") # I totally have NO CLUE. "luaconf.h:551:27: error: ISO C++ 1998 does not support 'long long' [-Werror=long-long]"

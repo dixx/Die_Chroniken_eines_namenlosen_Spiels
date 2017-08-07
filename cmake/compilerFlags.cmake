0f91a9b23fe69d05996af93c85dc5785e370b0bb
dixx <jenzdixx@googlemail.com>
2017-08-07 20:20:56 +0200

    Tidy up compilerFlags.cmake

--- cmake/compilerFlags.cmake
+++ cmake/compilerFlags.cmake
@@ -1,28 +1,28 @@
-set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3")
-
+# set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -c") # compile, but no linking
+# set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32")
+# set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Weverything") # applies only to clang compiler
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
+  -Weffc++ \
+  -Wall \
+  -Wextra \
+  -Wpedantic \
+  -Wunused \
+  -Werror \
+  -Wshadow \
+  -Winline \
+  -Wuninitialized \
+  -Wfloat-equal \
+  -Wconversion \
+  -Wsign-conversion \
+  -Wcast-align \
+  -Wno-aggregate-return \
+  -Woverloaded-virtual \
+  -Wold-style-cast \
+  -std=c++14 \
+  -fmessage-length=80 \
+  -fdiagnostics-color=always \
+  -fdiagnostics-show-location=once \
+")
 set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0 -g3")
 set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -D_DEBUG_MODE=1") # a switch for additional debug stuff in the code
-
-#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -c") # compile, but no linking
-#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32")
-# set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Weverything") # applies only to clang compiler
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Weffc++")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wextra")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wpedantic")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wunused")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wshadow")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Winline")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wuninitialized")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wfloat-equal")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wconversion")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wsign-conversion")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wcast-align")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-aggregate-return")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Woverloaded-virtual")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wold-style-cast")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fmessage-length=80")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fdiagnostics-color=always")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fdiagnostics-show-location=once")
+set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3")

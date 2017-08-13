b35628451a98b39e356a4597f7bd9c351dda50d9
dixx <jenzdixx@googlemail.com>
2017-08-13 10:19:07 +0200

    Fix CMake stringification

--- cmake/FindFakeIt.cmake
+++ cmake/FindFakeIt.cmake
@@ -1,5 +1,5 @@
 # TODO use https://github.com/philsquared/Catch/blob/master/docs/build-systems.md
-set(FAKEIT_INCLUDE_DIRS ${3RDPARTY_DIR}/fakeit)
+set(FAKEIT_INCLUDE_DIRS "${3RDPARTY_DIR}/fakeit")
 
 # to suppress warnings from libs:
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -isystem\"${FAKEIT_INCLUDE_DIRS}/\"")
--- cmake/FindIrrKlang.cmake
+++ cmake/FindIrrKlang.cmake
@@ -1,14 +1,14 @@
-set(IRRKLANG_DIR ${CMAKE_SOURCE_DIR}/dependencies/irrKlang-1.5.0)
-set(IRRKLANG_INCLUDE_DIRS ${IRRKLANG_DIR}/include)
+set(IRRKLANG_DIR "${CMAKE_SOURCE_DIR}/dependencies/irrKlang-1.5.0")
+set(IRRKLANG_INCLUDE_DIRS "${IRRKLANG_DIR}/include")
 
-if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
-  set(IRRKLANG_LIBRARY ${IRRKLANG_DIR}/lib/Win32-gcc/libirrKlang.a)
-  set(IRRKLANG_BINARY_LIBRARY ${IRRKLANG_DIR}/bin/Win32-gcc/irrKlang.dll)
-endif(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+if(${CMAKE_SYSTEM_NAME} MATCHES Windows)
+  set(IRRKLANG_LIBRARY "${IRRKLANG_DIR}/lib/Win32-gcc/libirrKlang.a")
+  set(IRRKLANG_BINARY_LIBRARY "${IRRKLANG_DIR}/bin/Win32-gcc/irrKlang.dll")
+endif(${CMAKE_SYSTEM_NAME} MATCHES Windows)
 
-if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
-  set(IRRKLANG_LIBRARY ${IRRKLANG_DIR}/bin/linux-gcc-64/libIrrKlang.so)
-endif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+if(${CMAKE_SYSTEM_NAME} MATCHES Linux)
+  set(IRRKLANG_LIBRARY "${IRRKLANG_DIR}/bin/linux-gcc-64/libIrrKlang.so")
+endif(${CMAKE_SYSTEM_NAME} MATCHES Linux)
 
 #if(APPLE)
 #endif(APPLE)

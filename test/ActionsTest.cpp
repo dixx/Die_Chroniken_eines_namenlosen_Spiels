dedb69f648baec628cc2db341c9d7d5a6bb1c346
dixx <jenzdixx@googlemail.com>
2018-06-04 00:37:21 +0200

    Update action mappings tests with BDD

--- test/ActionsTest.cpp
+++ test/ActionsTest.cpp
@@ -1,21 +1,73 @@
 #include <catch.hpp>
 #include "leviathan.h"
+#include "helpers/Testhelper.h"
 
-TEST_CASE( "Actions" ) {
-    leviathan::input::Actions subject;
+SCENARIO( "Leviathan Engine can map input to actions" ) {
+    Testhelper testhelper;
+    const irr::io::path mappingsFileName = "testactionmappings.yml";
+    irr::core::stringc content = "---\n";
+    content += "customizable:\n";
+    content += "  talk:\n";
+    content += "    id: 1\n";
+    content += "    name: initiate communication\n";
+    content += "    input_mappings:\n";
+    content += "      primary:\n";
+    content += "        name: left mouse button\n";
+    content += "        type: mouse\n";
+    content += "        id: 0x01\n";
+    content += "      secondary:\n";
+    content += "        name: <E>\n";
+    content += "        type: keyboard\n";
+    content += "        id: 0x45\n";
+    content += "  attack:\n";
+    content += "    name: main hand attack\n";
+    content += "    id: 2\n";
+    content += "    input_mappings:\n";
+    content += "internal:\n";
+    content += "  select:\n";
+    content += "    id: 100\n";
+    content += "    name: select a menu entry\n";
+    content += "    input_mappings:\n";
+    content += "      primary:\n";
+    content += "        name: <SPACE>\n";
+    content += "        type: keyboard\n";
+    content += "        id: 0x20\n";
+    testhelper.writeFile( mappingsFileName, content );
 
-    SECTION( "single action mapping" ) {
-        SECTION( "has attributes" ) {
-            // has name, primary, secondary, ...
+    GIVEN( "Leviathan Engine provides Actions" ) {
+        leviathan::input::Actions subject;
+
+        THEN( "it provides convenience methods" ) {
+            REQUIRE_FALSE( subject.inProgress( 0 ) );
+            REQUIRE( subject.inactive( 0 ) );
+            REQUIRE_FALSE( subject.justStarted( 0 ) );
+            REQUIRE_FALSE( subject.justStopped( 0 ) );
         }
-        SECTION( "can handle different input types" ) {}
+
+        WHEN( "action map is read from file" ) {
+            subject.mergeFromFile( mappingsFileName, testhelper.getFileSystem() );
+            enum { TALK = 1, ATTACK, SELECTION = 100 };
+            // send events
+
+            THEN( "it provides an action for each defined input mapping" ) {
+                REQUIRE_FALSE( subject.inProgress( ATTACK ) );
+                REQUIRE_FALSE( subject.inProgress( TALK ) );
+                REQUIRE_FALSE( subject.inProgress( SELECTION ) );
+
+                AND_THEN( "convenience methods are more helpfull" ) {
+                    REQUIRE( subject.justStarted( TALK ) );
+                    // send keyboard event
+                    REQUIRE( subject.inProgress( TALK ) );
+                    REQUIRE_FALSE( subject.inactive( TALK ) );
+                    REQUIRE_FALSE( subject.justStarted( TALK ) );
+                    REQUIRE_FALSE( subject.justStopped( TALK ) );
+                    // send keyboard event
+                    REQUIRE( subject.justStopped( TALK ) );
+                }
+                // AND_THEN( "actions have a state" ) {/*input1 || input2*/}
+            }
+            AND_THEN( "mappings can be overridden" ) {}
+            AND_WHEN( "no action is available" ) {}
+        }
     }
-
-    SECTION( "reads action map from game state" ) {
-        SECTION( "provides action for each defined input mapping" ) {}
-    }
-
-    SECTION( "reads action map from user profile" ) {}
-
-    SECTION( "error handling" ) {}
 }

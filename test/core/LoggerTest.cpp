0d40a7a2cdcfede04179004067255b28b5e7fe5e
dixx <jenzdixx@googlemail.com>
2019-03-31 13:56:09 +0200

    Tidy up LoggerTest

--- test/core/LoggerTest.cpp
+++ test/core/LoggerTest.cpp
@@ -4,92 +4,91 @@
 #include "irrlicht.h"
 #include <cstdint>
 
-TEST_CASE("Logger: init", "[unit]") {
+TEST_CASE("Logger", "[unit]") {
+    const char* logFileName = "testlogfile.log";
     Testhelper testhelper;
-    const irr::io::path logFileName = "testlogfile.log";
 
-    SECTION("it creates a logfile") {
-        leviathan::core::Logger sample(logFileName, leviathan::core::Logger::Level::INFO);
-        REQUIRE(testhelper.existFile(logFileName));
+    SECTION("init") {
+        SECTION("it creates a logfile") {
+            leviathan::core::Logger sample(logFileName, leviathan::core::Logger::Level::INFO);
+            REQUIRE(testhelper.existFile(logFileName));
 
-        uint32_t oldSize = testhelper.getFileSize(logFileName);
+            uint32_t oldSize = testhelper.getFileSize(logFileName);
 
-        SECTION("by overwriting an existing logfile") {
+            SECTION("by overwriting an existing logfile") {
+                leviathan::core::Logger subject(logFileName, leviathan::core::Logger::Level::INFO);
+                REQUIRE(testhelper.getFileSize(logFileName) == oldSize);
+            }
+
+            SECTION("by appending to an existing logfile") {
+                leviathan::core::Logger subject(logFileName, leviathan::core::Logger::Level::INFO, true);
+                REQUIRE(testhelper.getFileSize(logFileName) == oldSize * 2);
+            }
+
+            SECTION("and writes the global logging level into it") {
+                irr::core::stringc content = testhelper.readFile(logFileName);
+                REQUIRE(content.find("] LogLevel: Info") > -1);
+            }
+        }
+
+        SECTION("multiple logfiles may exist") {
+            const char* anotherLogFileName = "testlogfile2.log";
+            leviathan::core::Logger sample(logFileName, leviathan::core::Logger::Level::INFO);
+            leviathan::core::Logger sample2(anotherLogFileName, leviathan::core::Logger::Level::INFO);
+            REQUIRE(testhelper.existFile(logFileName));
+            REQUIRE(testhelper.existFile(anotherLogFileName));
+        }
+    }
+
+    SECTION("logging") {
+        SECTION("it writes text into the logfile") {
             leviathan::core::Logger subject(logFileName, leviathan::core::Logger::Level::INFO);
-            REQUIRE(testhelper.getFileSize(logFileName) == oldSize);
-        }
-
-        SECTION("by appending to an existing logfile") {
-            leviathan::core::Logger subject(logFileName, leviathan::core::Logger::Level::INFO, true);
-            REQUIRE(testhelper.getFileSize(logFileName) == oldSize * 2);
-        }
-
-        SECTION("and writes the global logging level into it") {
+            subject.text = "Kilroy wuz here";
+            subject.write();
             irr::core::stringc content = testhelper.readFile(logFileName);
-            REQUIRE(content.find("] LogLevel: Info") > -1);
-        }
-    }
+            REQUIRE(content.find("] Kilroy wuz here") > -1);
 
-    SECTION("multiple logfiles may exist") {
-        const irr::io::path anotherLogFileName = "testlogfile2.log";
-        leviathan::core::Logger sample(logFileName, leviathan::core::Logger::Level::INFO);
-        leviathan::core::Logger sample2(anotherLogFileName, leviathan::core::Logger::Level::INFO);
-        REQUIRE(testhelper.existFile(logFileName));
-        REQUIRE(testhelper.existFile(anotherLogFileName));
-    }
-}
+            SECTION("with special characters") {
+                subject.text = "german umlauts: äöüß, some other things: >_%&$§?!@|";
+                subject.write();
+                content = testhelper.readFile(logFileName);
+                REQUIRE(content.find("äöüß") > -1);
+                REQUIRE(content.find(">_%&$§?!@|") > -1);
+            }
 
-TEST_CASE("Logger: logging", "[unit]") {
-    Testhelper testhelper;
-    const irr::io::path logFileName = "testlogfile.log";
+            SECTION("over multiple lines") {
+                subject.text = "one line.";
+                subject.write();
+                subject.text = "another line.";
+                subject.write();
+                content = testhelper.readFile(logFileName);
+                int32_t firstIndex = content.find("line.");
+                REQUIRE(firstIndex > -1);
+                REQUIRE(content.find("line.", static_cast<uint32_t>(firstIndex) + 1) > -1);
+            }
 
-    SECTION("it writes text into the logfile") {
-        leviathan::core::Logger subject(logFileName, leviathan::core::Logger::Level::INFO);
-        subject.text = "Kilroy wuz here";
-        subject.write();
-        irr::core::stringc content = testhelper.readFile(logFileName);
-        REQUIRE(content.find("] Kilroy wuz here") > -1);
-
-        SECTION("with special characters") {
-            subject.text = "german umlauts: äöüß, some other things: >_%&$§?!@|";
-            subject.write();
-            content = testhelper.readFile(logFileName);
-            REQUIRE(content.find("äöüß") > -1);
-            REQUIRE(content.find(">_%&$§?!@|") > -1);
+            SECTION("and clears the text afterwards") {
+                subject.text = "usefull information";
+                subject.write();
+                REQUIRE(subject.text.empty());
+            }
         }
 
-        SECTION("over multiple lines") {
-            subject.text = "one line.";
-            subject.write();
-            subject.text = "another line.";
-            subject.write();
-            content = testhelper.readFile(logFileName);
-            int32_t firstIndex = content.find("line.");
-            REQUIRE(firstIndex > -1);
-            REQUIRE(content.find("line.", static_cast<uint32_t>(firstIndex) + 1) > -1);
+        SECTION("it handles different logLevels") {
+            leviathan::core::Logger subject(logFileName, leviathan::core::Logger::Level::DETAIL);
+            subject.text = "a line just for information";
+            subject.write(leviathan::core::Logger::Level::INFO);
+            subject.text = "a line full of details";
+            subject.write(leviathan::core::Logger::Level::DETAIL);
+            subject.text = "a line for debugging";
+            subject.write(leviathan::core::Logger::Level::DEBUG);
+            subject.text = "a line hopefully never ever written";
+            subject.write(leviathan::core::Logger::Level::ALL);
+            irr::core::stringc content = testhelper.readFile(logFileName);
+            REQUIRE(content.find("information") > -1);
+            REQUIRE(content.find("details") > -1);
+            REQUIRE_FALSE(content.find("debugging") > -1);
+            REQUIRE_FALSE(content.find("never ever") > -1);
         }
-
-        SECTION("and clears the text afterwards") {
-            subject.text = "usefull information";
-            subject.write();
-            REQUIRE(subject.text.empty());
-        }
-    }
-
-    SECTION("it handles different logLevels", "[unit]") {
-        leviathan::core::Logger subject(logFileName, leviathan::core::Logger::Level::DETAIL);
-        subject.text = "a line just for information";
-        subject.write(leviathan::core::Logger::Level::INFO);
-        subject.text = "a line full of details";
-        subject.write(leviathan::core::Logger::Level::DETAIL);
-        subject.text = "a line for debugging";
-        subject.write(leviathan::core::Logger::Level::DEBUG);
-        subject.text = "a line hopefully never ever written";
-        subject.write(leviathan::core::Logger::Level::ALL);
-        irr::core::stringc content = testhelper.readFile(logFileName);
-        REQUIRE(content.find("information") > -1);
-        REQUIRE(content.find("details") > -1);
-        REQUIRE_FALSE(content.find("debugging") > -1);
-        REQUIRE_FALSE(content.find("never ever") > -1);
     }
 }

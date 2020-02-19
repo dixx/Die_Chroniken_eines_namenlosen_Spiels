63feb6fdc0bc61825c75775f267288f7f7276eec
dixx <jenzdixx@googlemail.com>
2020-02-19 17:27:45 +0100

    Fix root menu element event reactivity

--- source/Leviathan/gui/Menu.cpp
+++ source/Leviathan/gui/Menu.cpp
@@ -4,16 +4,15 @@ namespace leviathan {
     namespace gui {
         Menu::Menu(irr::gui::IGUIEnvironment* guiEnv) : _guiEnv(guiEnv) {
             menuElement = _guiEnv->addModalScreen(_guiEnv->getRootGUIElement());
+            menuElement->setEnabled(false); // ignore clicks and buttons
         }
 
         void Menu::enable() {
-          menuElement->setEnabled(true);
-          menuElement->setVisible(true);
+            menuElement->setVisible(true);
         }
 
         void Menu::disable() {
-          menuElement->setEnabled(false);
-          menuElement->setVisible(false);
+            menuElement->setVisible(false);
         }
 
         void Menu::draw() {

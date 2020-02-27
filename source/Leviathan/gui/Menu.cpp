f5d7bfb6947f58e419129be3b3d37ceb7f83391c
dixx <jenzdixx@googlemail.com>
2020-02-27 19:13:28 +0100

    Add comment

--- source/Leviathan/gui/Menu.cpp
+++ source/Leviathan/gui/Menu.cpp
@@ -3,6 +3,7 @@
 namespace leviathan {
     namespace gui {
         Menu::Menu(irr::gui::IGUIEnvironment* guiEnv) : _guiEnv(guiEnv) {
+            // this only acts as a root element for a menu
             menuElement = _guiEnv->addModalScreen(_guiEnv->getRootGUIElement());
             menuElement->setEnabled(false); // ignore clicks and buttons
         }

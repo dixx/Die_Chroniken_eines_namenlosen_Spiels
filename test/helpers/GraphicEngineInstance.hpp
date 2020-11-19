9009cb4ca2decdaedb7802934b6f6800ee5f9ece
dixx <jenzdixx@googlemail.com>
2020-11-19 18:09:44 +0100

    Add generating of unique names for testing

--- test/helpers/GraphicEngineInstance.hpp
+++ test/helpers/GraphicEngineInstance.hpp
@@ -1,3 +1,6 @@
+#include <cstdint>
+#include <string>
+
 class GraphicEngineInstance {
 public:
     static irr::IrrlichtDevice* get() {
@@ -12,8 +15,14 @@ public:
         return graphicEngine_;
     }
 
+    static std::string uniqueName() {
+        static GraphicEngineInstance instance;
+        return std::to_string(instance.uniqueId++);
+    }
+
 private:
     irr::IrrlichtDevice* graphicEngine_ = nullptr;
+    uint32_t uniqueId = 0;
 
     GraphicEngineInstance() {
         irr::SIrrlichtCreationParameters params;

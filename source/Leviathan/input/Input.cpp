9a463d9a84c95df109d5ac6daf3da8ac9458dee6
dixx <jenzdixx@googlemail.com>
2021-01-09 14:52:35 +0100

    Use YAML defaulting instead of our own

--- source/Leviathan/input/Input.cpp
+++ source/Leviathan/input/Input.cpp
@@ -3,8 +3,8 @@
 namespace leviathan {
     namespace input {
         Input::Input(const YAML::Node& node)
-        : name(node && node["name"] ? node["name"].as<std::string>() : "- None -"),
-          type(node && node["type"] ? node["type"].as<std::string>() : "unknown"),
-          id(node && node["id"] ? node["id"].as<uint32_t>() : 0), isActive(false), wasActive(false) {}
+        : name(node ? node["name"].as<std::string>("- None -") : "- None -"),
+          type(node ? node["type"].as<std::string>("unknown") : "unknown"), id(node ? node["id"].as<uint32_t>(0) : 0),
+          isActive(false), wasActive(false) {}
     }
 }

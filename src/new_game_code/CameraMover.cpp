1b3425e807ccda9cc19ef38d7c00eab9bdf1f8b3
dixx <jenzdixx@googlemail.com>
2021-10-20 09:48:40 +0200

    Refactor CameraMover

--- src/new_game_code/CameraMover.cpp
+++ src/new_game_code/CameraMover.cpp
@@ -9,59 +9,50 @@ CameraMover::~CameraMover() {
 }
 
 void CameraMover::onAction(const leviathan::input::Action action) {
-    if (action.id == cameraActions::MOVE_FORWARD) {
-        isMovingForward_ = action.isActive;
-    } else if (action.id == cameraActions::MOVE_BACKWARD) {
-        isMovingBackward_ = action.isActive;
-    } else if (action.id == cameraActions::MOVE_LEFT) {
-        isMovingLeft_ = action.isActive;
-    } else if (action.id == cameraActions::MOVE_RIGHT) {
-        isMovingRight_ = action.isActive;
-    } else if (action.id == cameraActions::ROTATE_LEFT) {
-        isRotatingLeft_ = action.isActive;
-    } else if (action.id == cameraActions::ROTATE_RIGHT) {
-        isRotatingRight_ = action.isActive;
-    }
+    if (action.id < ROTATE_LEFT || action.id > MOVE_RIGHT) return;
+
+    actionActiveStates[action.id] = action.isActive;
 }
 
 bool CameraMover::isMoving() {
-    return isMovingForward_ || isMovingBackward_ || isMovingLeft_ || isMovingRight_;
+    return actionActiveStates[MOVE_FORWARD] || actionActiveStates[MOVE_BACKWARD] || actionActiveStates[MOVE_LEFT]
+           || actionActiveStates[MOVE_RIGHT];
 }
 
 bool CameraMover::isRotating() {
-    return isRotatingLeft_ || isRotatingRight_;
+    return actionActiveStates[ROTATE_LEFT] || actionActiveStates[ROTATE_RIGHT];
 }
 
 leviathan::video::Vector3D CameraMover::getMovementSpeed() {
     direction_.x = 0.f;
     direction_.z = 0.f;
-    if (isMovingForward_) direction_.z += 15.f;
-    if (isMovingBackward_) direction_.z -= 15.f;
-    if (isMovingLeft_) direction_.x -= 15.f;
-    if (isMovingRight_) direction_.x += 15.f;
+    if (actionActiveStates[MOVE_FORWARD]) direction_.z += MOVEMENT_SPEED;
+    if (actionActiveStates[MOVE_BACKWARD]) direction_.z -= MOVEMENT_SPEED;
+    if (actionActiveStates[MOVE_LEFT]) direction_.x -= MOVEMENT_SPEED;
+    if (actionActiveStates[MOVE_RIGHT]) direction_.x += MOVEMENT_SPEED;
     return direction_;
 }
 
 float CameraMover::getRotationSpeed() {
-    if (isRotatingLeft_ == isRotatingRight_) return 0.f;
+    if (actionActiveStates[ROTATE_LEFT] == actionActiveStates[ROTATE_RIGHT]) return 0.f;
 
-    return (isRotatingLeft_ ? 100.f : -100.f);
+    return (actionActiveStates[ROTATE_LEFT] ? ROTATION_SPEED : -ROTATION_SPEED);
 }
 
 void CameraMover::ignoreInput() {
-    actions_.unsubscribe(*this, cameraActions::ROTATE_LEFT);
-    actions_.unsubscribe(*this, cameraActions::ROTATE_RIGHT);
-    actions_.unsubscribe(*this, cameraActions::MOVE_FORWARD);
-    actions_.unsubscribe(*this, cameraActions::MOVE_BACKWARD);
-    actions_.unsubscribe(*this, cameraActions::MOVE_LEFT);
-    actions_.unsubscribe(*this, cameraActions::MOVE_RIGHT);
+    actions_.unsubscribe(*this, ROTATE_LEFT);
+    actions_.unsubscribe(*this, ROTATE_RIGHT);
+    actions_.unsubscribe(*this, MOVE_FORWARD);
+    actions_.unsubscribe(*this, MOVE_BACKWARD);
+    actions_.unsubscribe(*this, MOVE_LEFT);
+    actions_.unsubscribe(*this, MOVE_RIGHT);
 }
 
 void CameraMover::reactToInput() {
-    actions_.subscribe(*this, cameraActions::ROTATE_LEFT);
-    actions_.subscribe(*this, cameraActions::ROTATE_RIGHT);
-    actions_.subscribe(*this, cameraActions::MOVE_FORWARD);
-    actions_.subscribe(*this, cameraActions::MOVE_BACKWARD);
-    actions_.subscribe(*this, cameraActions::MOVE_LEFT);
-    actions_.subscribe(*this, cameraActions::MOVE_RIGHT);
+    actions_.subscribe(*this, ROTATE_LEFT);
+    actions_.subscribe(*this, ROTATE_RIGHT);
+    actions_.subscribe(*this, MOVE_FORWARD);
+    actions_.subscribe(*this, MOVE_BACKWARD);
+    actions_.subscribe(*this, MOVE_LEFT);
+    actions_.subscribe(*this, MOVE_RIGHT);
 }
